a2dcf4d19ca4b12f32471778587acf80c6a015ac
Mercy <aprilkasha42@gmail.com>
2020-06-21 21:59:57 +0300

    fix errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { NotFoundComponent } from './not-found/not-found.component';
 
+const routes: Routes = [
+  // { path: 'user', component: GithubComponent},
+  // { path: 'about', component: AboutComponent},
+  // { path:'**', component:NotFoundComponent},
+  //{ path: '', redirectTo:"/user", pathMatch:"full"},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import{RouterModule,Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: 'user', component: UserComponent},
+  {path: 'about', component: AboutComponent},
+  {path:  '', redirectTo: '/user', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+];
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRequestService } from './user-http/user-request.service';
+
 import { UserComponent } from './user/user.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DateCountPipe } from './date-count.pipe';
+import {RoutingModule} from './routing/routing-module';
 import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
@@ -19,14 +32,20 @@ import { HighlightDirective } from './highlight.directive';
     NavbarComponent,
     NotFoundComponent,
     DateCountPipe,
-    HighlightDirective
+    HighlightDirective,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule 
+    HttpClientModule,
+    FormsModule,
+    RoutingModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    UserRequestService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
